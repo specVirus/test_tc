3418dd0c854e5d21f5f46b0930087710e39f4a77
Шиллер <d.shiller@naibecar.com>
2018-08-16 16:04:30 +0700

    coverage.xml

---
+++ docker/init/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE yii2_iway_template;
+CREATE DATABASE yii2_iway_template_test;
+GRANT ALL PRIVILEGES ON DATABASE yii2_iway_template TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE yii2_iway_template_test TO postgres;
